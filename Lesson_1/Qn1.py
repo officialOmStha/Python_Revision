c6a6ae7bc7dbdb8f6feb673fa3daf8b02041816a
Om Official <omstha71@gmail.com>
2026-05-11 10:18:02 +0545

    First lession revision

---
+++ Lesson_1/Qn1.py
@@ -0,0 +1,30 @@
+# To display a string
+
+print("Hello, World!")
+
+# To take input and display them.
+
+name = input("Enter your Name:\n")
+age = int(input("Enter your age:\n"))
+faculty = input("Enter your faculty: \n")
+print(f"Name : {name}\nAge : {age}\nFaculty: {faculty}")
+
+# To create a virtual environment syntax :
+# In the terminal:
+# python -m venv env
+
+
+""" Multi line 
+comment """
+
+a = 20 
+b = 20
+print(f"Address of a : {id(a)}, Address of b : {id(b)}")
+
+
+#Arithmetic operator
+a = 40 - 20
+b = 4 * 5
+
+print(a, b)
+

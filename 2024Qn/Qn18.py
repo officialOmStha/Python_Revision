c10d17150815605bc021bfc366147483de6c6593
Om Official <omstha71@gmail.com>
2026-06-08 14:36:55 +0545

    solved qm 18 match case

---
+++ 2024Qn/Qn18.py
@@ -0,0 +1,21 @@
+# example of match case.
+
+uip = int(input("Enter a number from 1 to 7: "))
+
+match uip:
+    case 1:
+        print("Sunday")
+    case 2:
+        print("Monday")
+    case 3:
+        print("Tuesday")
+    case 4:
+        print("Wednesday")
+    case 5:
+        print("Thursday")
+    case 6:
+        print("Friday")
+    case 7:
+        print("Saturday")
+    case _:
+        print("Unexpected input, retry.")

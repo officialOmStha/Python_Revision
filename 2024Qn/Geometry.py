093d9fa8549203c1d6b35b025edc97b75366d02a
Om Official <omstha71@gmail.com>
2026-06-11 23:23:03 +0545

    module and use module

---
+++ 2024Qn/Geometry.py
@@ -0,0 +1,5 @@
+def carea(r):
+    return 3.14*r*r
+
+def recarea(l,b):
+    return l*b
---
+++ 2024Qn/use.py
@@ -0,0 +1,9 @@
+import Calculation
+import Geometry
+
+print(Calculation.add(4,5))
+print(Calculation.sub(22,5))
+print(Calculation.mul(22,5))
+print(Geometry.carea(7))
+print(Geometry.recarea(7,3))
+
